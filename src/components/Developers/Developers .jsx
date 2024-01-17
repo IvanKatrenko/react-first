c5ae5df11fbfbbe162cf76909f8c6d69de9203ee
ivankatrenko <ivankatrenko@gmail.com>
2024-01-17 23:26:27 +0100

    new file

---
+++ src/components/Developers/Developers .jsx
@@ -0,0 +1,22 @@
+import s from './Developers.module.scss'
+
+export const Developers = () => (
+    <ul className={s.developers}>
+        <li className={s.item}>
+            Designer :
+            <a className={s.link}
+                href="https://t.me/Mrshmallow"
+                target='_blank'
+                rel="noreferrer"> Anastasia Ilina
+            </a>
+        </li>
+        <li className={s.item}>
+            Developer :
+            <a className={s.link}
+                href="https://web.telegram.org/k/"
+                target='_blank'
+                rel="noreferrer"> Ivan Katrenko
+            </a>
+        </li>
+    </ul>
+)
